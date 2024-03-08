2280be7e663e3081e868309719b9a3620705fbba
viteshbava <82492234+viteshbava@users.noreply.github.com>
2024-03-08 14:06:16 +1300

    Add start of create artist logic

--- app/admin/[category]/artists/new/CreateArtistClient.tsx
+++ app/admin/[category]/artists/new/CreateArtistClient.tsx
@@ -8,11 +8,15 @@ import TextArea from '@/app/components/admin/inputs/TextArea';
 import YesNoSwitch from '@/app/components/admin/inputs/YesNoSwitch';
 import Heading from '@/app/components/admin/typography/Heading';
 import Button from '@/app/components/admin/ui/Button';
+import axios from 'axios';
+import { useRouter } from 'next/navigation';
 import React, { useState } from 'react';
-import { FieldValues, useForm } from 'react-hook-form';
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
+import toast from 'react-hot-toast';
 
 const CreateArtistClient = () => {
   const [isLoading, setIsLoading] = useState(false);
+  const router = useRouter();
 
   const {
     register,
@@ -23,6 +27,24 @@ const CreateArtistClient = () => {
     formState: { errors },
   } = useForm<FieldValues>();
 
+  const createArtist: SubmitHandler<FieldValues> = async (data) => {
+    setIsLoading(true);
+    try {
+      await axios.post('/api/artists', data);
+      toast.success('Artist created!');
+      router.refresh();
+      router.push('/admin/collections/artists');
+    } catch (error: any) {
+      console.error(error);
+      let message = error?.response?.data || '';
+      message =
+        message !== '' ? message : 'Cannot create artist right now. Please try again later.';
+      toast.error(message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
     <>
       <HeaderBar>
@@ -31,38 +53,38 @@ const CreateArtistClient = () => {
           <Button
             outline
             disabled={isLoading}
-            onClick={() => {}}
-            // onClick={() => {
-            //   reset(defaultValues);
-            // }}
-          >
+            onClick={() => router.push('/admin/collections/artists')}>
             Cancel
           </Button>
           <Button
             submit
             disabled={isLoading}
             className='ml-2'
-            onClick={() => {}}
-            // onClick={(e) => {
-            //   e.preventDefault();
-            //   handleSubmit(updateSection)(e);
-            // }}
-          >
+            onClick={(e) => {
+              e.preventDefault();
+              handleSubmit(createArtist)(e);
+            }}>
             Save
           </Button>
         </div>
       </HeaderBar>
       <form>
         <EditContainer>
-          <Input
-            id='name'
-            label='Name'
-            disabled={isLoading}
-            register={register}
-            errors={errors}
-            required
-          />
-          <div className='my-4 bg-yellow-300'>IMAGE INPUT GOES HERE!</div>
+          <div className='flex'>
+            <div className='basis-2/3 pr-6'>
+              <Input
+                id='name'
+                label='Name'
+                disabled={isLoading}
+                register={register}
+                errors={errors}
+                required
+              />
+            </div>
+
+            <div className='my-4 bg-yellow-300 basis-1/3'>IMAGE INPUT GOES HERE!</div>
+          </div>
+
           <TextArea
             id='description'
             label='Description'
@@ -83,6 +105,9 @@ const CreateArtistClient = () => {
               // onChange={(selection) => setSwitchVal(selection)}
             />
           </div>
+        </EditContainer>
+        <EditContainer heading='Artist Links'>
+          <div className='bg-yellow-300'>ARTIST LINK TABLE GOES HERE!</div>
         </EditContainer>
       </form>
     </>
---
+++ app/api/artists/route.ts
@@ -0,0 +1,60 @@
+import { NextResponse } from 'next/server';
+import prisma from '@/app/libs/prisma';
+import getCurrentUser from '@/app/actions/getCurrentUser';
+
+const validateObject = (obj: any) => {
+  const requiredStringProperties = ['name', 'description'];
+
+  // Check properties exist
+  [...requiredStringProperties].forEach((prop) => {
+    if (!obj.hasOwnProperty(prop))
+      throw new NextResponse(`Listing validation failed: '${prop}' not supplied.`, {
+        status: 400,
+        statusText: 'MISSING_VALUE',
+      });
+  });
+
+  // Check for non-empty strings
+  requiredStringProperties.forEach((prop) => {
+    if (typeof obj[prop] !== 'string' || obj[prop].trim() === '')
+      throw new NextResponse(`Listing validation failed: '${prop}' must be a valid string.`, {
+        status: 400,
+        statusText: 'EMPTY_STRING',
+      });
+  });
+
+  // All conditions passed, return the object
+  return obj;
+};
+
+export const POST = async (request: Request) => {
+  try {
+    const currentUser = await getCurrentUser();
+
+    if (!currentUser)
+      throw new NextResponse('Please log in first!', {
+        status: 400,
+        statusText: 'NO_CURRENT_USER',
+      });
+
+    const body = await request.json();
+    const { name, description } = validateObject(body);
+    const artist = await prisma.artist.create({
+      data: {
+        name,
+        description,
+      },
+    });
+
+    return NextResponse.json(artist);
+  } catch (error: any) {
+    console.error(error);
+
+    if (error?.statusText) return error;
+
+    return new NextResponse('Unable to create artist right now! Please try again later.', {
+      status: 400,
+      statusText: 'POST_LISTING_FAIL',
+    });
+  }
+};
