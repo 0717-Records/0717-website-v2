083225b3d5ca77d80ddb7d22084cf52e3f083169
viteshbava <82492234+viteshbava@users.noreply.github.com>
2024-04-11 18:16:43 +1200

    Add another test

--- app/components/sections/Explore.tsx
+++ app/components/sections/Explore.tsx
@@ -3,9 +3,15 @@ import Paragraph from '../Typography/Paragraph';
 import SectionWrapper from '../SectionWrapper';
 
 const Explore = async () => {
-  const data = await getExploreData();
-  if (!data) return null;
-  const { title, sub_title, artist_placeholder } = data;
+  // const data = await getExploreData();
+  // if (!data) return null;
+  // const { title, sub_title, artist_placeholder } = data;
+
+  const title = 'Explore';
+  const sub_title = 'We take pride.';
+  const artist_placeholder = {
+    message: 'Keen to collaborate?',
+  };
 
   return (
     <SectionWrapper title={title} subTitle={sub_title}>
