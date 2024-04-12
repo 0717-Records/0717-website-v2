88142a8596ad7d918f64a72a875590214e63f75e
viteshbava <82492234+viteshbava@users.noreply.github.com>
2024-04-12 17:45:00 +1200

    Add more responsive styles to hero

--- app/components/sections/Hero.tsx
+++ app/components/sections/Hero.tsx
@@ -17,10 +17,10 @@ const Hero = () => {
         priority
       />
       {/* <div className='absolute inset-0 bg-black opacity-40' /> */}
-      <div className='absolute inset-0 bg-gradient-to-t from-black to-transparent opacity-99' />
-      <div className='absolute bottom-20 md:bottom-20 right-1/2 md:right-44 text-center md:text-right text-white space-y-4 translate-x-1/2 md:translate-x-0'>
-        <h1 className='text-4xl font-bold shadow-md'>{title}</h1>
-        <p className='text-2xl shadow-md'>{subtitle}</p>
+      <div className='absolute inset-0 bg-gradient-to-t from-black from-10% to-transparent opacity-99' />
+      <div className='absolute bottom-20 md:bottom-20 right-1/2 lg:right-44 text-center lg:text-right text-white space-y-4 translate-x-1/2 lg:translate-x-0 w-full p-4 md:p-0'>
+        <h1 className='text-4xl sm:text-6xl font-bold shadow-md'>{title}</h1>
+        <p className='text-2xl sm:text-4xl shadow-md'>{subtitle}</p>
       </div>
     </div>
   );
