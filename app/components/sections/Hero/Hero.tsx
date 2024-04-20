e6465d9a1437e4d03504dc7838781f83e0a4b3f2
viteshbava <82492234+viteshbava@users.noreply.github.com>
2024-04-21 11:32:47 +1200

    Adjust heading on hero for mobile

--- app/components/sections/Hero/Hero.tsx
+++ app/components/sections/Hero/Hero.tsx
@@ -9,24 +9,6 @@ const Hero = async () => {
   const { main_title, sub_title, events, images } = data;
 
   const hasFeaturedContent = events.length > 0;
-  // const images = [
-  //   {
-  //     path: '/images/test-hero-images/hero-img-1.jpg',
-  //     alt: 'Star Control gig at Wine Cellar',
-  //   },
-  //   {
-  //     path: '/images/test-hero-images/hero-img-2.avif',
-  //     alt: 'Rock gig image 2',
-  //   },
-  //   {
-  //     path: '/images/test-hero-images/hero-img-3.avif',
-  //     alt: 'Rock gig image 3',
-  //   },
-  //   {
-  //     path: '/images/test-hero-images/hero-img-4.avif',
-  //     alt: 'Rock gig image 4',
-  //   },
-  // ];
 
   return (
     <section className={`relative ${styles['hero-height']} bg-black`}>
@@ -35,7 +17,7 @@ const Hero = async () => {
       {hasFeaturedContent ? (
         <FeaturedHeroContent title={main_title} subtitle={sub_title} events={events} />
       ) : (
-        <div className='absolute bottom-20 md:bottom-20 right-1/2 lg:right-44 text-center lg:text-right text-white space-y-4 translate-x-1/2 lg:translate-x-0 w-full p-4 md:p-0'>
+        <div className='absolute bottom-10 md:bottom-20 right-1/2 lg:right-44 text-center lg:text-right text-white space-y-4 translate-x-1/2 lg:translate-x-0 w-full p-4 md:p-0'>
           <h1 className='text-4xl sm:text-6xl font-bold'>{main_title}</h1>
           <p className='text-2xl sm:text-4xl'>{sub_title}</p>
         </div>
