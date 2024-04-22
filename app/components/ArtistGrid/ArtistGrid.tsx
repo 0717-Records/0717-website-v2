6ee090035f17ea3c36eb8248a1db37feb06d12f7
viteshbava <82492234+viteshbava@users.noreply.github.com>
2024-04-22 20:40:45 +1200

    Add a rough start to artist modals

--- app/components/ArtistGrid/ArtistGrid.tsx
+++ app/components/ArtistGrid/ArtistGrid.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { DisplayArtist } from '@/app/types';
 import React from 'react';
 import Image from 'next/image';
@@ -11,29 +13,45 @@ interface ArtistGridProps {
 }
 
 const ArtistGrid = ({ artists, placeholder = false, placeHolderText, email }: ArtistGridProps) => {
+  const openArtistModal = (name: string) => {
+    window.history.pushState(null, '', `/${name}`);
+    document.title = `07:17 Records - ${name}`;
+  };
+
+  const closeArtistModal = () => {
+    window.history.pushState(null, '', '/');
+    document.title = '07:17 Records';
+  };
+
   return (
     <div className='flex gap-6 md:gap-12 max-w-screen-md justify-center flex-wrap px-4'>
       {artists.map((artist) => (
-        <div className='flex flex-col justify-start items-center cursor-pointer sm:w-[35vw] sm:max-w-[10rem] sm:flex-grow sm:mx-2 group'>
+        <div
+          onClick={() => openArtistModal(artist.name)}
+          className='flex flex-col justify-start items-center cursor-pointer sm:w-[35vw] sm:max-w-[10rem] sm:flex-grow sm:mx-2 group'>
           <div className='w-32 h-32 overflow-hidden rounded-full mb-4 relative transition-all duration-200 ease-in-out sm:w-40 sm:h-40 group-hover:scale-[1.02]'>
             <Image
               fill
               src={artist.image || '/images/placeholder.jpg'}
               alt={artist.name}
               sizes='(min-width: 1024px) 30vw, (min-width: 640px) 35vw, 50vw'
+              className='object-cover'
             />
           </div>
           <h3 className='text-xl group-hover:font-semibold'>{artist.name}</h3>
         </div>
       ))}
       {placeholder && (
-        <div className='flex flex-col justify-start items-center sm:w-[35vw] sm:max-w-[10rem] sm:flex-grow sm:mx-2'>
+        <div
+          onClick={closeArtistModal}
+          className='flex flex-col justify-start items-center sm:w-[35vw] sm:max-w-[10rem] sm:flex-grow sm:mx-2'>
           <div className='w-32 h-32 overflow-hidden rounded-full mb-4 relative transition-all duration-200 ease-in-out sm:w-40 sm:h-40 bg-primary_yellow'>
             <Image
               fill
               src='/images/question-mark.webp'
               alt='Collaboration artist placeholder image'
               sizes='(min-width: 1024px) 30vw, (min-width: 640px) 35vw, 50vw'
+              className='object-cover'
             />
           </div>
           <h3 className='text-xl'>{placeHolderText}</h3>
