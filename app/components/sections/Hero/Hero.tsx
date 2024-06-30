330873b77520fba47c6c108d11d4f0f868f8dbeb
viteshbava <82492234+viteshbava@users.noreply.github.com>
2024-06-30 20:06:51 +0200

    Test hero without featured images

--- app/components/sections/Hero/Hero.tsx
+++ app/components/sections/Hero/Hero.tsx
@@ -15,20 +15,15 @@ const Hero = async () => {
       id='home'
       className={`relative ${styles['hero-height']} bg-black flex flex-col justify-center`}>
       <HeroImages images={images} />
-      {hasFeaturedContent ? (
+      {!hasFeaturedContent ? (
         <FeaturedHeroContent title={main_title} subtitle={sub_title} events={events} />
       ) : (
         <>
-          <div className='absolute inset-0 bg-black opacity-70 z-20' />
-          <div className='py-6 lg:py-2 relative text-white text-center flex flex-col justify-between items-center h-full z-30 border-4 border-blue-500'>
+          <div className='absolute inset-0 bg-gradient-to-t from-black from-20% to-transparent opacity-90 z-20' />
+          <div className='absolute z-30 bottom-10 md:bottom-20 right-1/2 lg:right-44 text-center lg:text-right text-white space-y-4 translate-x-1/2 lg:translate-x-0 w-full p-4 md:p-0'>
             <h1 className='text-4xl sm:text-6xl font-bold'>{main_title}</h1>
             <p className='text-2xl sm:text-4xl'>{sub_title}</p>
           </div>
-          {/* <div className='relative z-20 bottom-10 md:bottom-20 right-1/2 lg:right-44 text-center lg:text-right text-white space-y-4 translate-x-1/2 lg:translate-x-0 w-full p-4 md:p-0'>
-              <div className='absolute inset-0 bg-gradient-to-t from-black from-20% to-transparent opacity-90 z-20' />
-              <h1 className='text-4xl sm:text-6xl font-bold'>{main_title}</h1>
-              <p className='text-2xl sm:text-4xl'>{sub_title}</p>
-            </div> */}
         </>
       )}
     </section>
