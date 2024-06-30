065a404dcec61a2f9cb15d934beec3b99ed7b6a2
viteshbava <82492234+viteshbava@users.noreply.github.com>
2024-06-30 19:38:06 +0200

    Test hero

--- app/components/sections/Hero/FeaturedHeroContent.tsx
+++ app/components/sections/Hero/FeaturedHeroContent.tsx
@@ -15,14 +15,14 @@ const FeaturedHeroContent = ({ title, subtitle, events }: FeaturedHeroContentPro
       <div className='absolute inset-0 bg-black opacity-70 z-20' />
       <div
         className={`py-6 lg:py-2 relative text-white text-center flex flex-col justify-between items-center h-full z-30 border border-blue-500`}>
-        {/* <h1 className='text-3xl sm:text-6xl font-bold'>{title}</h1>
-        <div className='mt-8 flex flex-wrap flex-grow gap-4 justify-center sm:w-[85vw] w-[90vw] mx-auto'>
+        <h1 className='text-3xl sm:text-6xl font-bold'>{title}</h1>
+        <div className='mt-8 flex flex-wrap flex-grow gap-4 justify-center w-[80vw] mx-auto'>
           {events.map((event, index) => {
             if (index > 0) return <></>;
             return <EventContainer key={index} event={event} />;
           })}
         </div>
-        <p className='text-2xl sm:text-4xl my-2 sm:mt-0'>{subtitle}</p> */}
+        <p className='text-2xl sm:text-4xl my-2 sm:mt-0'>{subtitle}</p>
       </div>
     </>
   );
