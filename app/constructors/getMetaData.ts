57e943643fdddf8a941491fa25ecfe18c2718780
viteshbava <82492234+viteshbava@users.noreply.github.com>
2024-08-26 16:31:01 +0100

    Fix meta data in getMetaData

--- app/constructors/getMetaData.ts
+++ app/constructors/getMetaData.ts
@@ -11,9 +11,9 @@ const getMetaData = async () => {
 
   const getField = getFieldFunc(section);
 
-  const title = getField('meta_data', 'title') as string;
-  const description = getField('meta_data', 'description') as string;
-  const keywords = getField('meta_data', 'keywords') as string;
+  const title = getField('meta_data', 'meta_data>title') as string;
+  const description = getField('meta_data', 'meta_data>description') as string;
+  const keywords = getField('meta_data', 'meta_data>keywords') as string;
 
   return {
     title: title || '',
