c0b58ddb8466f233730c0762a9ec5c6da0411fb5
viteshbava <82492234+viteshbava@users.noreply.github.com>
2024-09-15 13:18:33 +0100

    Expose social image in getMetaData

--- app/constructors/getMetaData.ts
+++ app/constructors/getMetaData.ts
@@ -14,11 +14,13 @@ const getMetaData = async () => {
   const title = getField('meta_data', 'meta_data>title') as string;
   const description = getField('meta_data', 'meta_data>description') as string;
   const keywords = getField('meta_data', 'meta_data>keywords') as string;
+  const socialImgUrl = getField('meta_data', 'meta_data>social_img') as string;
 
   return {
     title: title || '',
     description: description || '',
     keywords: keywords || '',
+    socialImgUrl: socialImgUrl || '',
   };
 };
 
